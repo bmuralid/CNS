486fc0ffa19c60d5e2b38e2677cf579ec587a766
Balaji Muralidharan <bmuralidharan3@gatech.edu>
2025-09-12 17:09:08 -0400

    A working CMake build system for an AMReX application

---
+++ exec/main.cpp
@@ -0,0 +1,7 @@
+
+#include <cns.H>
+
+int main(int argc, char* argv[])
+{
+    return cns(argc, argv);
+}
---
+++ src/cns.cpp
@@ -0,0 +1,38 @@
+#include <cns.H>
+
+using namespace amrex;
+int cns(int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+
+    {
+        // timer for profiling
+        BL_PROFILE("main()");
+
+        // wallclock time
+        const auto strt_total = amrex::second();
+
+        // constructor - reads in parameters from inputs file
+        //             - sizes multilevel arrays and data structures
+        /* AmrCoreCNS amr_core_adv; */
+
+        // initialize AMR data
+        /* amr_core_adv.InitData(); */
+
+        // advance solution to final time
+        /* amr_core_adv.Evolve(); */
+
+        // wallclock time
+        auto end_total = amrex::second() - strt_total;
+
+        if (1) {
+        /* if (amr_core_adv.Verbose()) { */
+            // print wallclock time
+            ParallelDescriptor::ReduceRealMax(end_total ,ParallelDescriptor::IOProcessorNumber());
+            amrex::Print() << "\nTotal Time: " << end_total << '\n';
+        }
+    }
+
+    amrex::Finalize();
+    return 0;
+}
